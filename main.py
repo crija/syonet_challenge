64cd330f7dafcfe84ac454fedf3094c331358242
crija <crijacarla@gmail.com>
2023-10-17 14:37:39 -0300

    Adicionando linhas de documentação símples

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# Importando a bibliotéca pandas para trabalhar com o csv.
 import pandas as pd
 
-df = pd.read_csv("./data.csv", sep = ';')
+# Lendo o arquivo csv e passando segundo parâmentro para padronizar com ';'. Usando o 'a função 'head' para usar as 5 primeiras linhas no teste.
+df = pd.read_csv("./data.csv", sep = ';').head()
+
+# Escolhendo três colunas do arquivo.
 print(df[['idade', 'acoes', 'valor']])
+
