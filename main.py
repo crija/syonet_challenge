8beec88699449eba30b2541ec263de1ffcce3afd
crija <crijacarla@gmail.com>
2023-10-17 14:39:03 -0300

    Corrigindo escrita

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Importando a bibliotéca pandas para trabalhar com o csv.
 import pandas as pd
 
-# Lendo o arquivo csv e passando segundo parâmentro para padronizar com ';'. Usando o 'a função 'head' para usar as 5 primeiras linhas no teste.
+# Lendo o arquivo csv e passando segundo parâmentro para padronizar com ';'. Usando a função 'head' para usar as 5 primeiras linhas no teste.
 df = pd.read_csv("./data.csv", sep = ';').head()
 
 # Escolhendo três colunas do arquivo.
